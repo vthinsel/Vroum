55730efd924e7b9e3e2d979468245656962c6a95
Vince <Vince@TOUR-DEV-GAME>
2025-04-21 21:57:20 +0200

    Ajoutez des fichiers projet.

---
+++ Assets/Scripts/scrolling.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public class scrolling : MonoBehaviour
+{
+    [Range(0.0f, 3f)]
+    [SerializeField]
+    float speed = 1f;
+    Renderer rend;
+    float offset;
+    void Start()
+    {
+        rend = GetComponent<Renderer>();
+        //offset = 0;
+        //speed = 1f;
+    }
+
+    void Update()
+    {
+        //Vector2 vecoffset;
+        offset += (Time.deltaTime * speed);
+        //Debug.Log(offset + " " + Time.deltaTime + " "+ speed);
+        //rend.material.SetTextureOffset("_MainTex", new Vector2(0, offset));
+        rend.material.mainTextureOffset = new Vector2(0, -offset);
+        //vecoffset = rend.material.GetTextureOffset("_MainTex");
+        //Debug.Log("Current offset: " + vecoffset.x + " " + vecoffset.y);
+
+    }
+}
